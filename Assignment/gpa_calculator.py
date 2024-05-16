9ddabcad7b2e70ee1feb9d5fca516e90134ffaf5
Abir-0173 <tanimur196@gmail.com>
2024-05-16 20:42:33 +0600

    anothe assignment and django

--- Assignment/gpa_calculator.py
+++ Assignment/gpa_calculator.py
@@ -12,11 +12,19 @@ average of all subject from user
 
 
 '''
-Bangla  =int( input("Enter Your Number: "))
-english =int( input("Enter Your Number: "))
-math =int( input("Enter Your Number: "))
-science =int( input("Enter Your Number: "))
-print(Bangla,english, math, science)
+Bangla  =int( input("Enter Your Bangla Marks: "))
+if Bangla > 100:
+        print("Marks cannot be greater than 100.")
+english =int( input("Enter English Marks: "))
+if english > 100:
+        print("Marks cannot be greater than 100.")
+math =int( input("Enter Math Marks: "))
+if math > 100:
+        print("Marks cannot be greater than 100.")
+science =int( input("Enter Science Marks: "))
+if science > 100:
+        print("Marks cannot be greater than 100.")
+# print(Bangla,english, math, science)
 
 average_of_all_Subject = (Bangla + english + math + science)/4
 print(average_of_all_Subject)
@@ -37,7 +45,7 @@ def Calculation(b):
     elif b >= 91 and b <= 100:
         print("Your Grade is A+")
     else:
-        print("Invalid number given By user")
+        print("Invalid mark. Marks should be between 0 and 100.")
 
     # return inner(average_of_all_Subject)
 
